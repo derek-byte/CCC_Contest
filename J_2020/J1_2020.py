f6d705b7840b7ddeb2e7bee1ca768274c48d5225
derek-byte <derek.s.prog@gmail.com>
2022-02-08 22:40:34 -0500

    CCC 2020 J1

---
+++ J_2020/J1_2020.py
@@ -0,0 +1,11 @@
+small = int(input())
+medium = int(input())
+large = int(input())
+
+score = small + (2*medium) + (3*large)
+
+if score >= 10:
+    print("happy")
+
+else: 
+    print("sad")
--- J_2021/J4_2021.py
+++ J_2021/J4_2021.py
@@ -4,8 +4,15 @@ def arranging_books(books):
   j = 0
   while j < len(book):
     i = len(book) - 1
+
+    lowest_value = "S"
+    lowest_value_index = i
+
     while i > j:
       if book[i] < book[j]:
+        if book[i] < lowest_value:
+          lowest_value = book[i]
+          lowest_value_index = j
         book.insert(j, book[i])
         book.pop(i+1)
         book.insert(i+1, book[j+1])
