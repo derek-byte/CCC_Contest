a87c7fa530e84d3afc198718d4164b6c96cfded3
derek-byte <derek.s.prog@gmail.com>
2022-02-10 21:21:24 -0500

    CCC 2020 J4

---
+++ J_2020/J4_2020.py
@@ -0,0 +1,25 @@
+def answer():
+    # Variable names are swapped
+    cyclic_shift = input()
+    text = input()
+
+    i = 0
+    while i < len(text):
+
+        j = len(text) 
+        k = 0
+        while j < len(cyclic_shift):
+            if cyclic_shift[k:j] == text:
+                return "yes"
+            j += 1 
+            k += 1
+
+        # Rotate characters 
+        front_text_char = text[0]
+        text = text[1::]
+        text += front_text_char
+        i += 1 
+    return "no"
+
+print(answer())
+
